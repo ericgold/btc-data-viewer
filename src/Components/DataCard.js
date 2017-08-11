4b140073466528e056441d2654d1ec5d3b14f875
ericgold <egold05@gmail.com>
2017-08-10 18:51:38 -0700

    branch initial

--- src/Components/DataCard.js
+++ src/Components/DataCard.js
@@ -1,13 +1,17 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import DonorList from './DonorList';
 
 class DataCard extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
+			count: 0,
 			donors: []
 		};
 		this.getData = this.getData.bind(this);
+		this.updateCount = this.updateCount.bind(this);
+		this.createUrl = this.createUrl.bind(this);
 	}
 	// fetch method to get data
 	getData(url) {
@@ -16,19 +20,44 @@ class DataCard extends React.Component {
 								 .then(jsonData => this.setState({ donors: jsonData }))
 								 .catch(error => console.log(error));
 	}
+
+	updateCount(event) {
+		//console.log(event.target.value);
+		this.setState({
+			count: event.target.value
+		})
+		this.createUrl();
+	}
+
+	createUrl(props) {
+		const url = `${this.props.url}${this.state.count}/`;
+		this.getData(url);
+	}
 	
 	componentDidMount(props) { 
-		this.getData(this.props.url);
+		//this.getData(this.props.url);
+		this.createUrl(props);
 	}
 
 	render() {
 		return(
 			<div className="data-card">
 				<h1>{this.props.title}</h1>
+				<input 
+					type="number" 
+					placeholder="enter a number from 1-10"
+					min="0"
+					max="10"
+					onChange={this.updateCount}/>
 				<DonorList donors={this.state.donors} />
 			</div>
 		)
 	}
+}
+
+DataCard.propTypes = {
+	title: PropTypes.string,
+	url: PropTypes.string.isRequired
 }
 
 export default DataCard;
